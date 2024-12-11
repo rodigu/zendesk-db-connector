99c5149a402d9a8c762ce73c35e46ec50525e52f
rodigu <rmoraisu@protonmail.com>
2024-12-11 07:50:32 -0300

    chat credentials now dict, better formatting

--- zdbcon/chat.py
+++ zdbcon/chat.py
@@ -1,12 +1,12 @@
 from zdbcon.audit import ZenAudit
 
 class ZenChat(ZenAudit):
-    def __init__(self, cred_json='./auth/zendesk.json'):
+    def __init__(self, credentials: dict[str, str]):
         """Zendesk integration for chat logs
 
-        :param str cred_json: zendesk API credentials, defaults to './auth/zendesk.json'
+        :param dict[str, str] credentials: Zendesk API credentials dictionary
         """
-        super().__init__(cred_json=cred_json)
+        super().__init__(credentials=credentials)
         self.table = "ChatLogs"
 
     @staticmethod
@@ -17,7 +17,16 @@ class ZenChat(ZenAudit):
         :param int ticket_id:
         :return list: chat history list
         """
-        return [{"ticket_id": ticket_id, "id": f"{event['id']}-{idx}", "content": h, "chat_id": event['id']} for idx, h in enumerate(event['value']['history']) if h['type']=="ChatMessage"]
+        return [
+            {
+                "ticket_id": ticket_id,
+                "id": f"{event['id']}-{idx}",
+                "content": h,
+                "chat_id": event['id']
+            }
+            for idx, h in enumerate(event['value']['history'])
+                if h['type'] == "ChatMessage"
+        ]
 
     def get_chat_history(self, ticket_id: int) -> GeneratorExit:
         """Retrieves chat history for given `ticket_id` from the Zendesk Audit API
@@ -25,13 +34,18 @@ class ZenChat(ZenAudit):
         :param int ticket_id:
         :yield GeneratorExit[list[dict]]: yields a formatted chat history using `ZenChat.format_chat_history`
         """
-        return (h for audit in self.client.tickets.audits(ticket=ticket_id) for event in audit.events if event['type'] == 'ChatStartedEvent' for h in self.format_chat_history(event, ticket_id))
+        return (
+            h for audit in self.client.tickets.audits(ticket=ticket_id)
+            for event in audit.events
+                if event['type'] == 'ChatStartedEvent'
+            for h in self.format_chat_history(event, ticket_id)
+        )
 
     def append_ticket_chat(self, ticket_id: int):
         """Appends chat for `ticket_id` to ChatLogs table
 
         :param int ticket_id:
         """
-        self.vp(">\tAppending Ticket Chat")
+        self.vp("Appending Ticket Chat")
         for h in self.get_chat_history(ticket_id):
             self.append_obj(h, recache=False, force=False)
