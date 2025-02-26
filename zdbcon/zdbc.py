66dbb65f1304121fde3027d831ba0f4257ee06ed
rodigu <rmoraisu@protonmail.com>
2025-02-26 14:49:02 -0300

    #24 - last updated ticket fetching

--- zdbcon/zdbc.py
+++ zdbcon/zdbc.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import datetime
 from typing import Generator
+from dateutil import tz
 
 from zenpy import Zenpy
 from zenpy.lib.api_objects import Ticket, Audit
@@ -19,8 +20,14 @@ class ZDBC:
             token=self._credentials.token
         )
 
-    def fetch_last_updated_tickets(self, since_date: datetime) -> Generator[Ticket, None, None]:
-        pass
+    def fetch_last_updated_tickets(self, since_datetime: datetime, include: list[str]=['metric_sets']) -> Generator[Ticket, None, None]:
+        """Fetches last updated tickets since `since_datetime`, sorted by `updated_at` in ascending order.
+
+        :param datetime since_datetime: datetime to use as starting point for API GET
+        :yield Generator[Ticket, None, None]: ticket search generator
+        """
+        for ticket in self.client.search(type='ticket', updated_at_after=since_datetime.astimezone(tz.tzutc()), sort_by='updated_at', sort_order='asc'):
+            yield self.client.tickets(id=ticket.id, include=include)
 
     def fetch_ticket_audits(self, ticket: Ticket) -> Generator[Audit, None, None]:
         pass
