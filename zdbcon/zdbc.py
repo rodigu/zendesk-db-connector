0a379e625b77604a8759e224dd7ef8a2dfca64e2
rodigu <rmoraisu@protonmail.com>
2025-03-14 15:17:11 -0300

    #28 - moved flatten dict inside zdbc class

--- zdbcon/zdbc.py
+++ zdbcon/zdbc.py
@@ -10,8 +10,6 @@ from zenpy import Zenpy
 from zenpy.lib.api_objects import Ticket, Audit
 from zenpy.lib.proxy import ProxyDict
 
-from flatten_dictionary import flatten_dict
-
 from zdbcon.credentials import Credentials
 
 
@@ -104,7 +102,7 @@ class ZDBC:
         d.pop('events', None)
         if 'decoration' in d['metadata']:
             d['metadata']['decoration'].pop('links', None)
-        return flatten_dict(d, ['id', 'type'])
+        return ZDBC.flatten_dict(d, ['id', 'type'])
 
     @staticmethod
     def extract_field_events_from_audits(ticket_id: int, audits: list[Audit], field_name: str):
@@ -209,3 +207,45 @@ class ZDBC:
             'ticket_id': ticket_id,
             'changed_at': audit.created_at
         }
+
+    @staticmethod
+    def flatten_dict(d: dict, key: str|list[str]) -> dict:
+        """Recusively flattens a dictionary (dictionaries inside of dictionaries are also flattened).
+
+        Lists with dictionaries are turned into dicts with keys using `key`.
+
+        If `key` is a list, it attempts to use each item from the list in order as a key.
+
+        :param dict d: dictionary to be flattened
+        :param str | list[str] key: key or keys to be used in keying lists of dictionaries
+        :return dict: flatenned dictionary
+        """
+        return { k: ZDBC.flatten_dict_list(v, key) for k, v in d.items() }
+
+    @staticmethod
+    def flatten_dict_list(dlist: list[dict], key: str|list[str]) -> dict:
+        """Flattens list of dictionaries recursively
+
+        :param list[dict] dlist: list of dictionaries
+        :param str | list[str] key: key(s) to be extrated from the dictionaries and turned into dict keys
+        :return dict: flattened dictionary
+        """
+        wrong_type = lambda x: type(x)!=list and type(x)!=dict
+
+        if wrong_type(dlist) or (type(dlist)==list and len(dlist) > 0 and wrong_type(dlist[0])):
+            return dlist
+
+        if type(dlist)==dict:
+            return flatten_dict(dlist, key)
+
+        def choose_key(d: dict) -> str:
+            if type(key)==str: return key
+            for k in key:
+                if k in d: return k
+
+        flattened = { d[choose_key(d)]: ZDBC.flatten_dict_list(d, key) for d in dlist }
+
+        for v in flattened.values():
+            v.pop(choose_key(v))
+
+        return flattened
